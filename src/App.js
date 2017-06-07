a6bcb3a60bf5cd86d2e4ea84e99bd13327b9a4d5
Sebastian <krewllobster@gmail.com>
2017-06-07 16:57:37 -0400

    add first level elements before modal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,115 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import classNames from 'classnames';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipes: [
+        {
+          name: 'Spaghetti',
+          description: 'lorem ipsum dolor sit amet',
+          open: false,
+          parts: [
+            'noodles',
+            'sauce',
+            'meatballs'
+          ]
+        },
+      ],
+    }
+  }
+
   render() {
+
+    const {recipes} = this.state;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className = 'flex-container'>
+        <RecipeBox recipes = {recipes}/>
       </div>
     );
   }
+}
+
+const Recipe = ({item}) =>
+  <div className = 'card'>
+    <div className = 'card-header'>
+      <a className="btn btn-primary"
+        data-toggle="collapse"
+        href={`#${item.name}`}
+        aria-expanded="false"
+        aria-controls={`#${item.name}`}
+      >{item.name}</a>
+    </div>
+    <div className = 'collapse' id={item.name}>
+      <div className = 'card-block'>
+        <p className = 'card-text'>{item.description}</p>
+      </div>
+      <ul className = "list-group list-group-flush">
+        {item.parts.map(part => (
+          <li className = 'list-group-item'>{part}</li>
+        ))}
+      </ul>
+      <div className = 'card-footer'>
+        <Button
+          type = 'button'
+          value = 'delete'
+          onClick = {() => {}}
+          cName = 'btn btn-danger'
+        >
+          Delete
+        </Button>
+        <Button
+          type = 'button'
+          value = 'edit'
+          onClick = {() => {}}
+          cName = 'btn btn-secondary'
+        >
+          Edit
+        </Button>
+
+      </div>
+    </div>
+  </div>
+
+
+const RecipeBox = ({recipes}) =>
+  <div className = 'card main'>
+    <div className = 'card-header'>
+      <h4 className = 'card-title'>RecipeBox</h4>
+    </div>
+    <div className = 'card-block'>
+      {recipes.map(item => <Recipe item = {item} />)}
+    </div>
+    <div className = 'card-footer'>
+      <Button
+        value = 'addrecipe'
+        onClick = {() => {}}
+        cName = 'btn btn-info'
+      >
+        Add Recipe
+      </Button>
+    </div>
+  </div>
+
+
+const Button = ({type, selected, onClick, value, children, cName}) => {
+
+  const buttonClass = classNames(
+    cName,
+    {'button-active': selected === value}
+  )
+  return (
+    <button
+      className = {buttonClass}
+      onClick = {onClick}
+      value = {value}
+    >{children}</button>
+  )
 }
 
 export default App;
